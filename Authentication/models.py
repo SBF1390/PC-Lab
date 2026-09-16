26cec0686aa56bd27334d1827ff73716f246decf
SBF1390 <sbf1390@gmail.com>
2026-09-16 11:27:37 +0330

    UserBase Model Added

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, UserName, password=None, **extra_fields):
+        if not UserName:
+            raise ValueError("UserName is required.")
+
+        user = self.model(UserName=UserName, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, UserName, password=None, **extra_fields):
+        extra_fields["is_staff"] = True
+        extra_fields["is_superuser"] = True
+
+        return self.create_user(UserName, password, **extra_fields)
+
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    UserName = models.CharField(max_length=85, unique=True)
+    FName = models.CharField(max_length=75)
+    Email = models.EmailField(max_length=150, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "UserName"
+    REQUIRED_FIELDS = ["FName", "Email"]
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.UserName
